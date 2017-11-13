97201861ab988decae0610ba96b1c8d62cfe28b8
Chansik Yun <chansigi@gmail.com>
2017-11-13 11:34:08 +0900

    iOS

---
+++ SwiftExample/SwiftExample/main.swift
@@ -0,0 +1,30 @@
+//  main.swift
+import Foundation
+
+// 모든 클래스가 더 이상 NSObject의 자식인 것이 필수가 아닙니다.
+class Car: NSObject {
+    var color: Int = 0
+    var speed: Int = 0
+    
+    func go() {
+        print("go")
+    }
+    
+    static func ho() {
+        print("ho")
+    }
+}
+
+var p = Car()
+p.speed = 42
+print(p.speed)
+
+p.go()
+Car.ho()
+
+
+
+
+
+
+
