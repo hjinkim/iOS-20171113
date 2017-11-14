e1e68107e2aad2d48677538ca6eafe6f63eee78f
Chansik Yun <chansigi@gmail.com>
2017-11-14 16:51:01 +0900

    iOS

--- SampleApp-Swift/SampleApp-Swift/AppDelegate.swift
+++ SampleApp-Swift/SampleApp-Swift/AppDelegate.swift
@@ -16,12 +16,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // View를 생성하는 방법 2가지
         // 1. 직접 생성
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 320))
-        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.white
         window?.addSubview(view)
         
+        let button = UIButton(type: .infoLight)
+        button.frame.origin = CGPoint(x: 100, y: 350)
+        window?.addSubview(button)
         
+        let button2 = UIButton(type: .roundedRect)
+        button2.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button2.setTitle("Hello", for: .normal)
+        window?.addSubview(button2)
         
+        // addSubView를 마지막에 호출한 View가 가장 위에 있습니다.
+        //  => z order가 가장 높습니다.
         
+        // 2. Xcode - Interface Builder
+        
+    
         return true
     }
 
